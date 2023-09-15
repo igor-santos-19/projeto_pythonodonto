8c12381c6e419a455b4b79628b8b229d91baedea
Igor Santos <63085756+igor-santos-19@users.noreply.github.com>
2023-09-15 00:06:49 -0300

    arquivosinicias

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# projeto_pythonodonto
-Projeto de questionário com objetivo de analisar os dados que serão inseridos e com isso montar uma base de informações 
+# Projeto pythonOdonto
+
+## Projeto testando commit
+
+# teste do git 
+# mais um novo teste
+
---
+++ main.py
@@ -0,0 +1,52 @@
+import requests
+from tkinter import *
+
+
+janelaPrincipal = Tk()
+janelaPrincipal.geometry("400x400")
+janelaPrincipal.title("Questionario Odonto")
+
+texto_sobreProjeto = Label(janelaPrincipal, text="Projeto Questionario Big data")
+texto_sobreProjeto.pack()
+Digiteseunome = Label(janelaPrincipal, text="Digite o seu nome")
+Digiteseunome.pack()
+
+
+def DadosPessoais():
+    perguntaNome = Label(janelaPrincipal, text= 'Digite o seu nome: ')
+    perguntaNome.pack()
+
+    armazenaNome = Entry(perguntaNome, text= 'Digite o seu nome')
+    armazenaNome.pack()
+# comentario de teste2
+    botaoTeste = Button(janelaPrincipal, text="Iniciar", cursor='watch')
+    botaoTeste.pack()
+    
+
+
+def perguntasFormulario():
+    
+    pergunta01 = Label(janelaPrincipal, text='1) Pergunta numero  um sobre odonto selecione uma das respostas: ')
+    pergunta01.pack()
+    botaoRadio = Radiobutton(janelaPrincipal, text='resposta1', value='valorResposta1')
+    botaoRadio.pack()
+    botaoRadio_2 = Radiobutton(janelaPrincipal, text='resposta2', value='valorResposta2')
+    botaoRadio_2.pack()
+
+    pergunta02 = Label(janelaPrincipal, text='2) Pergunta numero  um sobre odonto selecione uma das respostas: ')
+    pergunta02.pack()
+    botaoRadio_3 = Radiobutton(janelaPrincipal, text='resposta1', value='valorResposta3')
+    botaoRadio_3.pack()
+    botaoRadio_4 = Radiobutton(janelaPrincipal, text='resposta2', value='valorResposta4')
+    botaoRadio_4.pack()
+
+def botaoEnviaFormulario():
+    botaoTeste = Button(janelaPrincipal, text="Enviar respostas", cursor='watch')
+    botaoTeste.pack()
+
+DadosPessoais()
+perguntasFormulario()
+botaoEnviaFormulario()
+
+janelaPrincipal.mainloop()
+
