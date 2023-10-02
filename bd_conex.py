f5b8c508230b3af6917d9af221485ca23722a1e0
Igor Santos <63085756+igor-santos-19@users.noreply.github.com>
2023-10-02 00:59:28 -0300

    Add files via upload

---
+++ bd_conex.py
@@ -0,0 +1,53 @@
+import sqlite3
+
+conexao = sqlite3.connect('odontodb')
+print('banco conectado sqlite', conexao)
+
+cursor = conexao.cursor()
+
+def cria_tabelas():
+    try:
+        cursor.execute( 
+            " CREATE TABLE pacientes (" 
+                        " id_pac INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+                        " nome_pac TEXT NOT NULL, "
+                        " idade_pac INTEGER NOT NULL, " 
+                        " cpf_pac VARCHAR(11) NOT NULL, "
+                        " telefone TEXT, "
+                        " obs_pac TEXT "
+                        " ); "
+                            )
+        print('Tabela pacientes criada com sucesso')
+        cursor.execute( 
+            " CREATE TABLE dentista (" 
+                        " id_dent INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+                        " nome_dent TEXT NOT NULL, "
+                        " idade_dent INTEGER NOT NULL, " 
+                        " cpf_dent VARCHAR(11) NOT NULL, "
+                        " telefone TEXT, "
+                        " obs_dent TEXT, "
+                        " especializacao TEXT "
+                        " ); "
+                            )
+
+        print('Tabela dentista com sucesso')
+
+        cursor.execute(" CREATE TABLE consultas ( "
+                    " id_con INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
+                    " dataConsulta DATE NOT NULL, "
+                    " id_pac INTEGER, "
+                    " id_dent INTEGER, "
+                    " atendimento TEXT, "
+                    " FOREIGN KEY (id_dent) REFERENCES dentista(id_dent), "
+                    " FOREIGN KEY (id_pac) REFERENCES pacientes(id_pac) );")
+        print("Tabela consulta criada com sucesso")
+    except:
+            print('As tabelas ja existem')
+
+def inseredentista(nome_dent, idade_dent, cpf_dent, telefone, obs_dent, especializacao):
+    cursor.execute("INSERT INTO dentista (nome_dent, idade_dent, cpf_dent, telefone, obs_dent, especializacao) VALUES (?, ?, ?, ?, ?, ?)",
+                    (nome_dent, idade_dent, cpf_dent, telefone, obs_dent, especializacao)) 
+    conexao.commit()
+
+
+
---
+++ tk_inter.py
@@ -0,0 +1,45 @@
+from tkinter import *
+
+root = Tk()
+
+textoEntrada1 = Label(root, text='Digite o nome do dentista').pack()
+entrada1 = Entry(root)
+entrada1.pack()
+
+textoEntrada2 = Label(root, text='Digite a idade do dentista').pack()
+entrada2 = Entry(root)
+entrada2.pack()
+
+textoEntrada3 = Label(root, text='Digite o CPF do dentista').pack()
+entrada3 = Entry(root)
+entrada3.pack()
+
+textoEntrada4 = Label(root, text='Digite o Telefone do dentista').pack()
+entrada4 = Entry(root)
+entrada4.pack()
+
+textoEntrada5 = Label(root, text='Digite a Especialização do dentista').pack()
+entrada5 = Entry(root)
+entrada5.pack()
+
+textoEntrada6 = Label(root, text='Digite alguma Observação do dentista').pack()
+entrada6 = Entry(root)
+entrada6.pack()
+
+def bt_botaoEnviarDados():
+    import bd_conex
+    bd_conex.cria_tabelas()
+
+    nome_dent = entrada1.get()
+    idade_dent = entrada2.get()
+    CPF_dent = entrada3.get()
+    telefone_dent = entrada4.get()
+    Especializacao_dent = entrada5.get()
+    Obs_dent = entrada6.get()
+    
+    bd_conex.inseredentista(nome_dent, idade_dent, CPF_dent, telefone_dent, Especializacao_dent, Obs_dent)
+
+botaoEnviarDados = Button(root, text='Enviar Dados', command = bt_botaoEnviarDados).pack()
+
+root.geometry("400x400")
+root.mainloop()
