a7f4fb59fd9dc5385272499075d0c7742a1b6e70
Igor Santos <63085756+igor-santos-19@users.noreply.github.com>
2023-11-12 19:37:05 -0300

    Add files via upload

--- bd_conex.py
+++ bd_conex.py
@@ -1,5 +1,6 @@
 import sqlite3
 import pandas as pd
+from datetime import datetime
 
 conexao = sqlite3.connect('odontodb')
 print('banco conectado sqlite', conexao)
@@ -30,31 +31,108 @@ def cria_tabelas():
                         " especializacao TEXT "
                         " ); "
                             )
-
         print('Tabela dentista com sucesso')
-
         cursor.execute(" CREATE TABLE consultas ( "
                     " id_con INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, "
                     " dataConsulta DATE NOT NULL, "
-                    " id_pac INTEGER, "
-                    " id_dent INTEGER, "
-                    " atendimento TEXT, "
-                    " FOREIGN KEY (id_dent) REFERENCES dentista(id_dent), "
-                    " FOREIGN KEY (id_pac) REFERENCES pacientes(id_pac) );")
+                    " paciente TEXT, "
+                    " dentista TEXT, "
+                    " atendimento TEXT ); "
+                      )
         print("Tabela consulta criada com sucesso")
     except:
             print('As tabelas ja existem')
+# insere dados na tabela
 
 def inseredentista(nome_dent, idade_dent, cpf_dent, telefone, obs_dent, especializacao):
     cursor.execute("INSERT INTO dentista (nome_dent, idade_dent, cpf_dent, telefone, obs_dent, especializacao) VALUES (?, ?, ?, ?, ?, ?)",
-                    (nome_dent, idade_dent, cpf_dent, telefone, obs_dent, especializacao)) 
+                    (nome_dent, idade_dent, cpf_dent, telefone, especializacao, obs_dent )) 
     conexao.commit()
 
+def inserepacientenobd(nome_paciente, idade_paciente, cpf_paciente, telefone_paciente, obs_paciente):
+     cursor.execute('INSERT INTO pacientes (nome_pac, idade_pac, cpf_pac, telefone, obs_pac) VALUES(?,?,?,?,?)',
+                    (nome_paciente, idade_paciente, cpf_paciente, telefone_paciente, obs_paciente))
+     conexao.commit()
 
-dados = pd.read_sql('SELECT * FROM dentista', conexao)
-idade = pd.read_sql('SELECT SUM(idade_dent) FROM dentista',conexao )
+def insereconsultabd(tipoDeAtendimento, dataDaConsulta, nomePaciente, nomeDentista):
+     cursor.execute('INSERT INTO consultas (atendimento, dataConsulta, paciente, dentista) VALUES (?, ?, ?, ?)',
+                    (tipoDeAtendimento, dataDaConsulta, nomePaciente, nomeDentista))
+     conexao.commit()
+    
+#analise dos frames pandas
 
-mediaIdade = idade / 4
+def mediaIdadeDentista():
+    idade = pd.read_sql('SELECT SUM(idade_dent) FROM dentista',conexao ).iloc[0, 0]
+    selecionaMaximoId = pd.read_sql('SELECT COUNT(id_dent) FROM dentista', conexao).iloc[0, 0]
 
-print("As idade são ",dados)
-print("A media das idades é ", mediaIdade)
+    if selecionaMaximoId > 0:
+        mediaIdade = float(idade) / float(selecionaMaximoId)
+        print("A media das idades dos dentista é", "{:.2f}".format(mediaIdade))
+        return mediaIdade
+    else:
+        print('Não existe nenhum valor por isso nao e possivel tirar media das idades ')
+        
+
+def mediaIdadePacientes():
+    idade = pd.read_sql('SELECT SUM(idade_pac) FROM pacientes',conexao ).iloc[0, 0]
+    selecionaMaximoId = pd.read_sql('SELECT COUNT(id_pac) FROM pacientes', conexao).iloc[0, 0]
+
+    if selecionaMaximoId > 0:
+        mediaIdade = float(idade) / float(selecionaMaximoId)
+        print("A media das idades dos pacientes é: ", "{:.2f}".format(mediaIdade))
+        return mediaIdade
+    else:
+        print('Não existe nenhum valor por isso nao e possivel tirar media das idades ')
+        
+
+def especializacaoDent():   
+    TotalClinicoGeral = pd.read_sql('SELECT count(especializacao) from dentista where especializacao = "Clinico Geral"', conexao).iloc[0, 0]
+    print("Existem um total de ClinicoGeral dentistas", TotalClinicoGeral)
+    return TotalClinicoGeral
+
+def especializacaoDent1():
+    TotalOrtodontia = pd.read_sql('SELECT count(especializacao) from dentista where especializacao = "Ortodontia"', conexao).iloc[0, 0]
+    print("Existem um total de Ordontntia dentistas", TotalOrtodontia)
+    return TotalOrtodontia
+
+def tiposdeAtendPac():
+    TotalAtendiPac = pd.read_sql('SELECT count(atendimento) from consultas where atendimento = "Limpeza e exames Dentários"', conexao).iloc[0, 0]
+    print(' Existem um total de Limpeza e exames Dentarios', TotalAtendiPac)
+    return TotalAtendiPac
+
+def ultimaConsultaMarcada():
+    query = pd.read_sql("SELECT dataConsulta FROM consultas ORDER BY id_con DESC LIMIT 1", conexao).iloc[0, 0]
+
+    data_timestamp = datetime.strptime(query, "%m/%d/%y")
+    data_formatada = data_timestamp.strftime("%d/%m/%Y")
+    return data_formatada
+
+def ultimaConsultaMarcadaPaciente():
+    query2 = pd.read_sql("SELECT paciente FROM consultas ORDER BY id_con DESC LIMIT 1", conexao).iloc[0, 0]
+    return query2
+
+def ultimaConsultaMarcadDentista():
+     query3 = pd.read_sql("SELECT dentista FROM consultas ORDER BY id_con DESC LIMIT 1", conexao).iloc[0, 0]
+     return query3
+
+def SelecionaPacientes():
+    todosPac = pd.read_sql("SELECT nome_pac FROM PACIENTES", conexao)['nome_pac'].tolist()
+    return todosPac
+
+def SelecionaDentista():
+    todosDent = pd.read_sql("SELECT nome_dent FROM DENTISTA", conexao)['nome_dent'].tolist()
+    return todosDent
+
+def TotaldeConsultas():
+    todasconsultas = pd.read_sql("SELECT COUNT(*) as total_tuplas FROM consultas", conexao).iloc[0, 0]
+    return todasconsultas
+
+def TotaldeDentistas():
+    todosdentistas = pd.read_sql("SELECT COUNT(*) as total_tuplas FROM dentista", conexao).iloc[0, 0]
+    return todosdentistas
+
+def TotaldePacientes():
+    todospacientes = pd.read_sql("SELECT COUNT(*) as total_tuplas FROM pacientes", conexao).iloc[0, 0]
+    return todospacientes
+
+cria_tabelas()
--- tk_inter.py
+++ tk_inter.py
@@ -1,47 +1,146 @@
 from tkinter import *
+from tkinter import ttk
+from tkcalendar import Calendar
+from ttkthemes import ThemedTk
+import bd_conex
 
-root = Tk()
+def criar_entrada_label(root, texto, coluna, linha, rowspan=1, padx=5, pady=2, entry_width=30):
+    estilo_label = {'font': ('Helvetica', 10), 'fg': 'black'}
+    estilo_entrada = {'font': ('Helvetica', 10), 'bg': 'lightgray', 'fg': 'black', 'width': entry_width,
+                      'borderwidth': 0, 'relief': 'flat'}
+    label = Label(root, text=texto, **estilo_label)
+    label.grid(column=coluna, row=linha, rowspan=rowspan, padx=padx, pady=pady, sticky=W)
+    entrada = Entry(root, **estilo_entrada)
+    entrada.grid(column=coluna, row=linha + 1, rowspan=rowspan, padx=padx, pady=pady, sticky=W)
 
-textoEntrada1 = Label(root, text='Digite o nome do dentista').pack()
-entrada1 = Entry(root)
-entrada1.pack()
+    return entrada
 
-textoEntrada2 = Label(root, text='Digite a idade do dentista').pack()
-entrada2 = Entry(root)
-entrada2.pack()
+def criar_combobox_label(root, texto, opcoes, coluna, linha, padx=5, pady=2):
+    label = Label(root, text=texto)
+    label.grid(column=coluna, row=linha, padx=padx, pady=pady, sticky=W)
+    combobox = ttk.Combobox(root, values=opcoes)
+    combobox.grid(column=coluna, row=linha + 1, padx=padx, pady=pady, sticky=W)
+    return combobox
 
-textoEntrada3 = Label(root, text='Digite o CPF do dentista').pack()
-entrada3 = Entry(root)
-entrada3.pack()
+def criar_botao(root, texto, comando, coluna, linha, padx=5, pady=2):
+    botao = Button(root, text=texto, command=comando, font=("Arial", 12), background="#4CAF50", fg="white", relief=GROOVE)
+    botao.grid(column=coluna, row=linha, padx=padx, pady=pady, sticky="ew")
+    return botao
 
-textoEntrada4 = Label(root, text='Digite o Telefone do dentista').pack()
-entrada4 = Entry(root)
-entrada4.pack()
+def analiseDadosMostrarNatela():
+    total_atendimentos = bd_conex.tiposdeAtendPac()
+    totalMediaIdadePac = bd_conex.mediaIdadePacientes()
+    totalEscpecializacaoDent = bd_conex.especializacaoDent()
+    totalMediaIdadeDEnt = bd_conex.mediaIdadeDentista()
+    totalOrtodontia = bd_conex.especializacaoDent1()
+    totalDeconsultas = bd_conex.TotaldeConsultas()
+    TotaldeDentistas = bd_conex.TotaldeDentistas()
+    TotaldePacientes = bd_conex.TotaldePacientes()
 
-textoEntrada5 = Label(root, text='Digite a Especialização do dentista').pack()
-entrada5 = Entry(root)
-entrada5.pack()
+    textoDesaidaa0 = Label(root, text=f'Total de Dentistas: {TotaldeDentistas}')
+    textoDesaidaa0.grid(column=4, row=3, padx=2, pady=2)
 
-textoEntrada6 = Label(root, text='Digite alguma Observação do dentista').pack()
-entrada6 = Entry(root)
-entrada6.pack()
+    textoDesaidaa1 = Label(root, text=f'Total de Pacientes: {TotaldePacientes}')
+    textoDesaidaa1.grid(column=4, row=4, padx=2, pady=2)
+
+    textoDesaida = Label(root, text=f'Total de atendimentos: {totalDeconsultas}')
+    textoDesaida.grid(column=4, row=5, padx=2, pady=2)
+
+    textoDesaida0 = Label(root, text=f'Atendimentos relacionados a Ortodontia: {totalOrtodontia}')
+    textoDesaida0.grid(column=4, row=6, padx=2, pady=2)
+
+    textoDesaida = Label(root, text=f'Atendimentos relacionados a Limpeza e exames Dentários: {total_atendimentos}')
+    textoDesaida.grid(column=4, row=7, padx=2, pady=2)
+
+    textoDesaida1 = Label(root, text=f'A media das idades dos pacientes é: {"{:.2f}".format(totalMediaIdadePac)}')
+    textoDesaida1.grid(column=4, row=8, padx=2, pady=2)
+
+    textoDesaida2 = Label(root, text=f'Total de ClinicoGeral: {totalEscpecializacaoDent}')
+    textoDesaida2.grid(column=4, row=9, padx=2, pady=2)
+
+    textoDesaida3 = Label(root, text=f'A media das idades dos dentista é: {"{:.2f}".format(totalMediaIdadeDEnt)}')
+    textoDesaida3.grid(column=4, row=10, padx=2, pady=2)
 
 def bt_botaoEnviarDados():
-    import bd_conex
-    bd_conex.cria_tabelas()
-
     nome_dent = entrada1.get()
     idade_dent = entrada2.get()
     CPF_dent = entrada3.get()
     telefone_dent = entrada4.get()
     Especializacao_dent = entrada5.get()
     Obs_dent = entrada6.get()
-    
+
     bd_conex.inseredentista(nome_dent, idade_dent, CPF_dent, telefone_dent, Especializacao_dent, Obs_dent)
 
-botaoEnviarDados = Button(root, text='Enviar Dados', command = bt_botaoEnviarDados).pack()
+def bt_botaoEnviarDadosPac():
+    nome_paciente = entrada7.get()
+    idade_paciente = entrada8.get()
+    cpf_paciente = entrada9.get()
+    telefone_paciente = entrada10.get()
+    obs_paciente = entrada11.get()
 
+    bd_conex.inserepacientenobd(nome_paciente, idade_paciente, cpf_paciente, telefone_paciente, obs_paciente)
 
+def botaoEnviaDadosConsulta():
+    tipoDeAtendimento = entrada13.get()
+    dataDaConsulta = calendarioConsulta.get_date()
+    nomePaciente = entrada14.get()
+    nomeDentista = entrada15.get()
 
-root.geometry("400x400")
+    bd_conex.insereconsultabd(tipoDeAtendimento, dataDaConsulta, nomePaciente, nomeDentista)
+
+    ultimaconsulta = bd_conex.ultimaConsultaMarcada()
+    paciente = bd_conex.ultimaConsultaMarcadaPaciente()
+    dentista = bd_conex.ultimaConsultaMarcadDentista()
+
+    textoultimaconsulta = Label(root, text=f'Ultima Consulta marcada para o dia: {ultimaconsulta} ')
+    textoultimaconsulta2 = Label(root, text=f'Paciente: {paciente} ')
+    textoultimaconsulta3 = Label(root, text=f'Dentista: {dentista} ')
+
+    textoultimaconsulta.grid(column=3, row=15, padx=5, pady=2)
+    textoultimaconsulta2.grid(column=3, row=16, padx=5, pady=2)
+    textoultimaconsulta3.grid(column=3, row=17, padx=5, pady=2)
+root = ThemedTk(theme="arc")
+root.title("Sistema de Odontologia")
+
+#entradas e combobox para dentistas
+entrada1 = criar_entrada_label(root, 'Digite o nome do dentista', 1, 1, rowspan=3)
+entrada2 = criar_entrada_label(root, 'Digite a idade do dentista', 1, 4)
+entrada3 = criar_entrada_label(root, 'Digite o CPF do dentista', 1, 6)
+entrada4 = criar_entrada_label(root, 'Digite o Telefone do dentista', 1, 8)
+entrada5 = criar_combobox_label(root, 'Digite a Especialização do dentista',
+                                ["Clinico Geral", "Ortodontia", "Implantodontia", "Traumatologia", "Periodontia",
+                                 "Endodontia", "Dentistica", "Harmonização Orofacial"], 1, 10)
+entrada6 = criar_entrada_label(root, 'Digite alguma Observação do dentista', 1, 12)
+
+#entradas para pacientes
+entrada7 = criar_entrada_label(root, 'Digite o nome do Paciente', 2, 1, rowspan=3)
+entrada8 = criar_entrada_label(root, 'Digite a idade do Paciente', 2, 4)
+entrada9 = criar_entrada_label(root, 'Digite o CPF do Paciente', 2, 6)
+entrada10 = criar_entrada_label(root, 'Digite o Telefone do Paciente', 2, 8)
+entrada11 = criar_entrada_label(root, 'Digite alguma Observação do Paciente', 2, 10)
+
+#calendario e combobox para consulta
+calendarioConsulta = Calendar(root, selectmode='day', year=2023, month=10, day=21)
+calendarioConsulta.grid(column=3, row=2, padx=5, pady=2, rowspan=5)
+
+entrada13 = criar_combobox_label(root, 'Selecione o tipo do atendimento',
+                                 ["Limpeza e exames Dentários", "Tratamento de Caries",
+                                  "Tratamento de Gengivite e Periodontite", "Tratamento de Canal Radicular",
+                                  "Extrações Dentárias", "Protese Dentárias", "Ortodontia", "Implantes Dentários",
+                                  "Odontopediatria", "Odontologia Estetica", "Tratamento de disfunção",
+                                  "Odontologia Preventiva", "Cirurgia Oral", "Odontologia Geriatrica"], 3, 7)
+
+todospac = bd_conex.SelecionaPacientes()
+todosdent = bd_conex.SelecionaDentista()
+
+entrada14 = criar_combobox_label(root, 'Pacientes', todospac, 3,9 )
+entrada15 = criar_combobox_label(root, 'Dentista', todosdent, 3,11 )
+
+#botoes
+analiseDados_button = criar_botao(root, 'Analisar Dados e Mostrar na Tela', analiseDadosMostrarNatela, 4, 14)
+botaoEnviarDados = criar_botao(root, 'Enviar Dados do dentista', bt_botaoEnviarDados, 1, 14)
+botaoEnviarDadosPac = criar_botao(root, 'Enviar Dados do paciente', bt_botaoEnviarDadosPac, 2, 14)
+botaoEnviarDadosConst = criar_botao(root, 'Enviar Dados da Consulta', botaoEnviaDadosConsulta, 3, 14)
+
+root.geometry("1100x500")
 root.mainloop()
